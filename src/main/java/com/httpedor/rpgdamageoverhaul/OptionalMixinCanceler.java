c71e855e50b21572564cd74e5f06bb448807c434
httpedor <httpedor.dev@gmail.com>
2025-06-28 16:47:24 -0300

    Fixed crash when bettercombat, soulsweapons or simplyswords isn't loaded.

--- src/main/java/com/httpedor/rpgdamageoverhaul/OptionalMixinCanceler.java
+++ src/main/java/com/httpedor/rpgdamageoverhaul/OptionalMixinCanceler.java
@@ -1,6 +1,8 @@
 package com.httpedor.rpgdamageoverhaul;
 
 import com.bawnorton.mixinsquared.api.MixinCanceller;
+import net.minecraftforge.fml.ModList;
+import net.minecraftforge.fml.loading.LoadingModList;
 
 import java.util.List;
 
@@ -8,8 +10,16 @@ public class OptionalMixinCanceler implements MixinCanceller {
 
     @Override
     public boolean shouldCancel(List<String> targetClassNames, String mixinClassName) {
-        if (!mixinClassName.contains("data/rpgdamageoverhaul"))
+        if (!mixinClassName.contains("rpgdamageoverhaul"))
             return false;
+
+        var instance = LoadingModList.get();
+        var deps = List.of("soulsweapons", "bettercombat", "simplyswords");
+        for (var dep : deps)
+        {
+            if (instance.getModFileById(dep) == null && mixinClassName.contains(dep))
+                return true;
+        }
         return false;
     }
 }
